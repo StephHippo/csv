e82a98396931f5dac44b8e05bba36a33ac9e9173
stephhippo <stephanie.hippo@gmail.com>
2013-11-11 12:57:10 -0500

    Updated main.rb to use STDIN/OUT.

--- main.rb
+++ main.rb
@@ -4,32 +4,13 @@ require './EntryValidator.rb'
 
 begin
 
-  #unless ARGV[0].nil? || ARGV[1].nil?
-  #  k = ARGV[0].to_i
-  #  file = ARGV[1]
-  #else
-  #  puts "What is the value of k?"
-  #  k = gets
-  #  k = k.to_i
-  #  puts "What is the name of the file?"
-  #  file = gets
-  #  file = file.delete("\n")
-  #end
-
-
-  #get file passed from STDIN
-  file = "testcases/example.txt"
-  log = Logger.new('log.txt')
-
-  raise "File not found" unless File.exists?(file)
-  f = File.open(file)
   #read in each line
-  f.each_line do |line|
+  STDIN.each_line do |line|
     #create validator object, pass in line
     val = EntryValidator.new(line)
     if val.validate_entry == "VALID"
       entry = Entry.new(val)
-      print entry.toCSV
+      STDOUT.print entry.toCSV
     end
   end
 
