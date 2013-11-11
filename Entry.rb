9e93d2ccd22c6e93e760f34656f2f9b54a6c5fdc
stephhippo <stephanie.hippo@gmail.com>
2013-11-11 00:33:58 -0500

    Fixed defect in status updating for gradSem

--- Entry.rb
+++ Entry.rb
@@ -1,18 +1,18 @@
 class Entry
-  attr_reader :status
+
+  @@log = Logger.new('log.txt')
 
   @@status = {
       :INITIALIZED => "INITIALIZED",
       :VALID => "VALID",
       :INVALID => "INVALID",
       :BADID => "ID OUTSIDE RANGE",
-      :NOID => "NO ID FOUND",
       :BADNAME => "MISSING PART OR ALL OF THE NAME",
       :BADSHORT => "SHORT NAME IS MISSING",
       :BADGRADSEM => "DOES NOT FOLLOW SPRING/FALL YEAR FORMAT"
   }
 
-  def initialize(line, log)
+  def initialize(line)
     @line = line
     @fields = line.split(" ")
     @id = 0
@@ -22,65 +22,39 @@ class Entry
     @short = ""
     @gradSem = ""
     @status = @@status[:INITIALIZED]
-    @log = log
-  end
-
-  #public methods check and sanitize data
-  def validate_entry
-    if @line.match(/[\d]{7}\s+[A-Za-z]*\s+[A-Za-z]*\s+([A-Za-z]*\s+)?[A-Za-z]*\s+F13\s+(S|F)\d\d/)
-      set_fields
-      @status = @@status[:VALID]
-    else
-      validate_fields
-      @log.info("#{@status} : #{@line}") unless @status == "VALID"
-    end
   end
 
   def toCSV
+    validate_entry
     if @status == @@status[:VALID]
-      @log.info("Successfully converted #{@line} to CSV format")
+      @@log.info("Successfully converted #{@line} to CSV format")
       "#{@id},#{@last.capitalize},#{@first.capitalize},#{@middle.capitalize},#{@short.downcase},#{@gradSem}\n"
     else
-      @log.info("#{@status}: #{@line}")
       ""
     end
   end
 
   private
 
-  def set_fields
-    @id = @fields.first
-    names = @fields[1...@fields.length-2]
-    set_names(names)
-    @gradSem = @fields.last
-  end
-
-  def set_names(names)
-    if names.length == 3
-      @last = names[0]
-      @first = names[1]
-      @short = names[2]
-      @status = @@status[:VALID]
-    elsif names.length == 4
-      @last = names[0]
-      @first = names[1]
-      @middle = names[2]
-      @short = names[3]
+  def validate_entry
+    if @line.match(/[\d]{7}\s+[A-Za-z]*\s+[A-Za-z]*\s+([A-Za-z]*\s+)?[A-Za-z]*\s+F13\s+(S|F)\d\d/)
+      set_fields
       @status = @@status[:VALID]
     else
-      @status = @@status[:BADNAME]
+      sanitize_fields
+      @@log.info("#{@status} : #{@line}") unless @status == "VALID"
     end
   end
 
   #validate the id field, the name fields, and the grad semester
-  def validate_fields
-    validate_id
-    validate_names
-    validate_grad_sem
+  def sanitize_fields
+    sanitize_id
+    sanitize_names
+    sanitize_grad_sem
   end
 
   #shouldn't rescue ids as we would be unable to guarantee they would remain unique without performance repercussions
-  def validate_id
+  def sanitize_id
     id = @fields.first
     if @status == @@status[:INITIALIZED]
       if id.length == 7
@@ -94,16 +68,49 @@ class Entry
   end
 
   #rescue and sanitize what we can
-  #TODO: Not always guaranteed to solve. condition the changing of :VALID set
-  def validate_names
+  def sanitize_names
     if @status == @@status[:VALID]
-      @status = @@status[:BADNAME]
       names = @line.scan(/\d\s.*\s.*\s.*\s.*F13/)[0].split(" ")
       names.delete(names.last)
       names.delete(names.first)
       names = remove_invalid_chars(names)
       set_names(names)
-      @log.info("WARNING: Attempted rescue of name values from #{names} to last: #{@last}, first: #{@first}, middle: #{@middle}, short: #{@short}.")
+      @@log.info("WARNING: Attempted rescue of name values from #{names} to last: #{@last}, first: #{@first}, middle: #{@middle}, short: #{@short}.")
+    end
+  end
+
+  #look for alternate inputs that could mean Fall or Spring and appropriate year
+  def sanitize_grad_sem
+    if @status == @@status[:VALID]
+      gradsem = @fields.last
+      (gradsem =~ /[F|S]\d\d/) ? @gradSem = gradsem : @status = @@status[:BADGRADSEM]
+      map_grad_sem(gradsem)
+    end
+  end
+
+  def set_fields
+    @id = @fields.first
+    names = @fields[1...@fields.length-2]
+    set_names(names)
+    @gradSem = @fields.last
+  end
+
+  def set_names(names)
+    #if there is no middle name
+    if names.length == 3
+      @last = names[0]
+      @first = names[1]
+      @short = names[2]
+      @status = @@status[:VALID]
+      #when there is a middle name
+    elsif names.length == 4
+      @last = names[0]
+      @first = names[1]
+      @middle = names[2]
+      @short = names[3]
+      @status = @@status[:VALID]
+    else
+      @status = @@status[:BADNAME]
     end
   end
 
@@ -115,34 +122,19 @@ class Entry
     names
   end
 
-  #look for alternate inputs that could mean Fall or Spring
-  def validate_grad_sem
-    if @status == @@status[:VALID]
-      gradsem = @fields.last
+  #try to map the gradsem input values to valid fields
+  def map_grad_sem(gradsem)
+    semester_map = ["Fall", "fall", "f", "F", "Spring", "spring", "s", "S"]
 
-      #assume we won't rescue the gradsem
-      if gradsem =~ /[F|S]\d\d/
-        @gradSem = gradsem
-      else
-        semesterMap = {"Fall" => "F", "fall" => "F", "f" => "F",
-                       "Spring" => "S", "spring" => "S", "s" => "S"}
-        @status = @@status[:BADGRADSEM]
-
-        #if we can rescue the grad sem, reset the status
-        semesterMap.each do |k,v|
-          if gradsem.include? k
-            sem = v
-            yr = gradsem.scan(/\d\d/).last
-            @gradSem = "#{sem}#{yr}"
-            @log.info("WARNING: Attempted rescue of gradSem from #{gradsem} to #{@gradSem}")
-            @status = @@status[:VALID]
-          end
+    if @status == @@status[:BADGRADSEM]
+      semester_map.each do |sem|
+        if (gradsem.start_with? sem) && (@status != @@status[:VALID])
+          semester = sem[0].upcase
+          yr = gradsem.scan(/\d\d/).last
+          @gradSem = "#{semester}#{yr}"
+          @@log.info("WARNING: Attempted rescue of gradSem from #{gradsem} to #{@gradSem}")
+          @status = @@status[:VALID]
         end
-      end
-      if @status != @@status[:VALID]
-        @gradSem = "UNKNOWN"
-        @status = @@status[:VALID]
-        @log.info("WARNING: Unable to identify the graduation semester, set to unknown.")
       end
     end
   end
