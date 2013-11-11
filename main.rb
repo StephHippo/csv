efece7e1372029f0dd20c7451b18217ed5fe780b
stephhippo <stephanie.hippo@gmail.com>
2013-11-11 00:34:27 -0500

    Moved log and cleaned up main.

--- main.rb
+++ main.rb
@@ -7,21 +7,13 @@ begin
   file = "testcases/example.txt"
   log = Logger.new('log.txt')
 
-  if File.exists?(file)
-    #open file
-    f = File.open(file)
-    #read in each line
-    f.each_line do |line|
-      #create validator object, pass in line
-      entry = Entry.new(line, log)
-      entry.validate_entry
-      puts entry.toCSV
-    end
-  else
-    log.info("#{file} is not a valid file.")
+  raise "File not found" unless File.exists?(file)
+  f = File.open(file)
+  #read in each line
+  f.each_line do |line|
+    #create validator object, pass in line
+    entry = Entry.new(line)
+    print entry.toCSV
   end
-
-  #close logger
-  log.close
 
 end
